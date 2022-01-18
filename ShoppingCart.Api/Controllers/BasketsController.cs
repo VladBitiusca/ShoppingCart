3230a26ad697b1d3151d0427d995b29a94398632
Vlad Bitiusca <vlad.bitiusca@tss-yonder.com>
2022-01-18 10:20:32 +0200

    Initial commit - architecture and gitignore

---
+++ ShoppingCart.Api/Controllers/BasketsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ShoppingCart.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateBasket()
+        {
+
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}/article-line")]
+        public IActionResult UpdateBasket([FromRoute]Guid id)
+        {
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBasket([FromRoute]Guid id)
+        {
+
+            return Ok();
+        }
+    }
+}
